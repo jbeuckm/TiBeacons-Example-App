36bb5d8342843bb167a9b522fa3b45c90679f7d9
joe beuckman <joebeuckman@gmail.com>
2013-11-10 11:57:24 -0600

    notify for near vs immediate

--- app/lib/bgService.js
+++ app/lib/bgService.js
@@ -1,12 +1,16 @@
 var TiBeacons = require('org.beuckman.tibeacons');
-Ti.API.info("module is => " + TiBeacons);
 
 TiBeacons.addEventListener("beaconRanges", function(event) {
     
     for (var i in event.beacons) {
         var b = event.beacons[i];
-        if (b.proximity == "immediate") {
-            notify("Found a beacon in immediate proximity!");
+        switch (b.proximity) {
+            case "immediate":
+                notify("Found "+beacon.major+"/"+beacon.minor+" in immediate proximity!");
+                break;
+            case "near":
+                notify("Found "+beacon.major+"/"+beacon.minor+" in near proximity!");
+                break;
         }
     }
 
